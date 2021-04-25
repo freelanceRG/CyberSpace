5bf6ddbae117c8d95e2a0d8ea71de04a13034180
freelanceRG <83164618+freelanceRG@users.noreply.github.com>
2021-04-25 14:02:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.about__slider').slick({
+		dots:true,
+	});
+
+	$('.esports__games-slider').slick({
+		dots:true,
+  		slidesToShow: 2,
+  		slidesToScroll: 2
+	});
+
+	$('.teams__slider').slick({
+		dots:true,
+	});
+});
